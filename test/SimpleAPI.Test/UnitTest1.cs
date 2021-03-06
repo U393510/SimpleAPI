a36c8360ab1ae28820a189dc78510330ff00e1c7
Mohit <mohitxp@yahoo.com>
2021-03-06 21:04:23 +0530

    Initial Commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+       
+        //SimpleAPI
+ 
+
+        
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
